1704ac2e7948e81d3d786f4b2fd3e923615004e6
aleInIyan <143300980+aleInIyan@users.noreply.github.com>
2023-12-21 22:13:34 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,18 +6,18 @@ SSID="YourSSID"
 # Wireless password (if your network is secured)
 PASSWORD="YourPassword"
 
-# Wireless interface name (replace with your actual wireless interface, e.g., wlan0)
-INTERFACE="wlan0"
+# Wireless interface name
+INTERFACE="wlp3s0"
 
 # Connect to the wireless network
-ifconfig $INTERFACE up
-iwconfig $INTERFACE essid "$SSID" key s:"$PASSWORD"
+ip link set $INTERFACE up
+iw dev $INTERFACE connect -w "$SSID" key 0:"$PASSWORD"
 
 # Obtain an IP address using DHCP (if needed)
 dhclient $INTERFACE
 
 # Check if the connection is successful
-if ifconfig $INTERFACE | grep -q "inet"; then
+if ip a show $INTERFACE | grep -q "inet"; then
   echo "Connected to $SSID successfully!"
 else
   echo "Failed to connect to $SSID. Check your settings."
